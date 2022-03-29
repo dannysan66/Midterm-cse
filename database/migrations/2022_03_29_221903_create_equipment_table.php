0cfad31a7db4d0dbe68224b9175a58c0f8d53a5d
dannysan66 <34259485+dannysan66@users.noreply.github.com>
2022-03-29 16:37:59 -0700

    Add files via upload

---
+++ database/migrations/2022_03_29_221903_create_equipment_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipment', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('specs');
+            $table->string('price');
+            $table->id('category', ['desktop', 'laptop', 'phone', 'tablet']);
+            $table->foreignId('manufacture_id')->constrained('manufacture');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipment');
+    }
+};
