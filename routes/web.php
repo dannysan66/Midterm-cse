0b230ac7d86eb34aebcf7bc054e0263c86d9b367
dannysan66 <ordonezd6@gmail.com>
2022-03-28 20:09:55 -0700

    first

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/db-migrate', function () {
+    Artisan::call('migrate');
+    echo Artisan::output();
+});
+
+Route::get('/db-test', function () {
+    try {
+         echo \DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+          echo 'None';
+    }
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/employees', function () {
+    return view('employees');
+});
+
+Route::get('/hardware', function () {
+    return view('hardware');
+});
+
+Route::get('/notes', function () {
+    return view('notes');
+});
