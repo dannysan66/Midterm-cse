44451031a24a05cc348451f19d45d099ed9abfb9
dannysan66 <34259485+dannysan66@users.noreply.github.com>
2022-03-30 14:57:42 -0700

    Update list.blade.php

--- resources/views/customer/list.blade.php
+++ resources/views/customer/list.blade.php
@@ -1,25 +1,25 @@
 @extends('adminlte::page')
 
-@section('title', 'Customers')
+@section('title', 'Buyers')
 
 @section('content_header')
-    <h1>Customers</h1>
+    <h1>List</h1>
 @stop
 
 @section('content')
 <div class="card">
   <div class="card-body">
-    <table id="table" class="table table-bordered">
+    <table id="table" class="table table-bordered sortable">
       <thead>
         <tr>
-          <th style="width: 10px">#</th><th>Task</th><th style="width: 40px">Action</th>
+          <th style="width: 10px">#</th><th>Name</th><th style="width: 40px">Action</th>
         </tr>
       </thead>
       <tbody>
         @foreach($customer AS $customer)
         <tr>
           <td>{{ $customer->id }}</td>
-          <td>{{ $customer->fullname }}</td>
+          <td>{{ $customer->full_name }}</td>
           <td><a class="btn btn-default btn-sm" href="{{ route('customer.show',['customer'=>$customer->id]) }}">View</a></td>
         </tr>
         @endforeach
@@ -28,4 +28,13 @@
   </div>
 </div>
 <a href="{{ route('customer.create') }} " class="btn btn-primary" >Create</a>
+
+
+
+@stop
+
+
+
+@section('js')
+<script src="https://www.kryogenix.org/code/browser/sorttable/sorttable.js"></script>
 @stop
