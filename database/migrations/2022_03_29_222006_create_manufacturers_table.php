32b2ba239f7f04a132d3cde7cc72d6ccfd74c344
dannysan66 <34259485+dannysan66@users.noreply.github.com>
2022-03-29 17:54:50 -0700

    Update and rename 2022_03_29_222006_create_manufacture_table.php to 2022_03_29_222006_create_manufacturers_table.php

--- database/migrations/2022_03_29_222006_create_manufacturers_table.php
+++ database/migrations/2022_03_29_222006_create_manufacturers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('manufacture', function (Blueprint $table) {
+        Schema::create('manufacturers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('name');
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('manufacture');
+        Schema::dropIfExists('manufacturers');
     }
 };
