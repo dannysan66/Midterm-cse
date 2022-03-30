d86ff1bc170f01230dca122a57eb6c1651f895a9
dannysan66 <34259485+dannysan66@users.noreply.github.com>
2022-03-30 13:10:53 -0700

    Add files via upload

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $customer = Customer::all();
+        return view('customer.list', compact('customer'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('customer.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+     public function store(Request $request)
+     {
+         $validated = $request->validate([
+              'full_name' => 'required',
+              'email' => 'required',
+              'phone_number' => 'required',
+         ]);
+
+         $customer = Customer::create([
+              'full_name' => $request->full_name, 
+              'email' => $request->email,
+              'phone_number' => $request->phone_number,
+         ]);
+
+         return $this->index();
+     }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $customer = Customer::find($id);
+        return view('customer.detail', compact('customer'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
