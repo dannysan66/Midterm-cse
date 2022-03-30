49988265d82881afbabfbf51d620b293103e3e8f
dannysan66 <34259485+dannysan66@users.noreply.github.com>
2022-03-30 14:58:09 -0700

    Update details.blade.php

--- resources/views/customer/details.blade.php
+++ resources/views/customer/details.blade.php
@@ -1,9 +1,9 @@
 @extends('adminlte::page')
 
-@section('title', 'Customers')
+@section('title', 'Buyers')
 
 @section('content_header')
-    <h1>Customer</h1>
+    <h1>Buyers</h1>
 @stop
 
 @section('content')
@@ -11,25 +11,15 @@
   <div class="card-body">
     <div>
         <div class="h1">
-            {{ $customer->fullname }}
+            {{ $customer->full_name }}
         </div>
         <dl class="row">
+            <dt class="col-sm-3">Phone Number</dt>
+            <dd class="col-sm-9">{{ $customer->phone_number }}</dd>
+
             <dt class="col-sm-3">Email</dt>
             <dd class="col-sm-9">{{ $customer->email }}</dd>
-
-            <dt class="col-sm-3">Phone Number</dt>
-            <dd class="col-sm-9">{{ $customer->phonenumber }}</dd>
         </dl>
     </div>
-    <span style="float:right;">
-        <a href="{{ route('customer.create') }} " class="btn btn-warning">Update</a>
-        <a href="{{ route('customer.destroy',['customer'=>$customer->id]) }}" class="btn btn-danger" onclick="event.preventDefault(); document.getElementById('submit-form').submit();">
-        Delete</a>
-        <form id="submit-form" action="{{ route('customer.destroy',['customer'=>$customer->id]) }}" method="POST" class="hidden">
-            @csrf
-            @method('DELETE')
-        </form>
-    </span>
-  </div>
-</div>
 @stop
+
