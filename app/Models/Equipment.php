695e1da204b2ad84de250f292101b5f1125b6427
dannysan66 <34259485+dannysan66@users.noreply.github.com>
2022-03-30 21:34:15 -0700

    Update Equipment.php

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Invoice;
+use App\Models\Notes;
+use App\Models\Manufacturers;
 
 class Equipment extends Model
 {
