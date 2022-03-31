abba67714a3654c8636d8b0d7bca4053c752e8b8
dannysan66 <34259485+dannysan66@users.noreply.github.com>
2022-03-30 18:28:21 -0700

    Add files via upload

---
+++ app/Forms/InvoiceForm.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Forms;
+
+use Kris\LaravelFormBuilder\Form;
+use Kris\LaravelFormBuilder\Field;
+
+class InvoiceForm extends Form
+{
+    public function buildForm()
+    {
+      $this->
+      add('invoice_num', Field::TEXT, [
+        'rules' => 'required',
+        'label' => 'Name'
+      ])
+      ->add('price', Field::TEXT, [
+        'rules' => 'required',
+        'label' => 'Price'
+      ])
+      ->add('purchase_date', Field::DATE, [
+        'rules' => 'required',
+        'label' => 'Purchase Date',
+      ])
+      ->add('equipment_id', Field::NUMBER, [
+        'rules' => 'required',
+        'label' => 'Equipment',
+      ])
+      ->add('customer_id', Field::NUMBER, [
+        'rules' => 'required',
+        'label' => 'Customer',
+      ])
+
+      ->add('submit', 'submit', [
+        'label' => 'Submit'
+      ]);
+    }
+}
